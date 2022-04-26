05e089ea2e2d571428de19e1f0dc1208d0042df9
Jessica <jesvadtz@gmail.com>
2022-04-25 22:29:57 -0500

    Connect to database

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const server = require("./src/server");
+const dbConnect = require("./src/lib/db");
+
+const PORT = process.env.PORT || 8080;
+
+dbConnect()
+  .then(() => {
+    console.log("Connect to database");
+    server.listen(PORT, () => {
+      console.log("Server running on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log("Error", error);
+  });
